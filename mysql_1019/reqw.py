2bf7d2a8402722502649dec7ac36f9aa68c39336
19982081092 <1114..cxf>
2022-05-24 17:41:43 +0800

    info

--- mysql_1019/reqw.py
+++ mysql_1019/reqw.py
@@ -139,19 +139,19 @@
 #
 # print(fac(6))
 
-# def fib(n):
-#     if n==1:
-#         return 1
-#     elif n==2:
-#         return 1
-#     else:
-#         return fib(n-1)+fib(n-2)  #fib(5)+fib(4)
-# #斐波那契数列第6位上的数字
-# print(fib(6))
-# print('------------------------------')
-# #输出这个数列的前6位上的数字
-# for i in range(1,7):
-#     print(fib(i))
+def fib(n):
+    if n==1:
+        return 1
+    elif n==2:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)  #fib(5)+fib(4)
+#斐波那契数列第6位上的数字
+print(fib(6))
+print('------------------------------')
+#输出这个数列的前6位上的数字
+for i in range(1,7):
+    print(fib(i))
 
 # names = ['张三','张晓霞','李旺明','陈晓旭','阿莫西林']
 #
